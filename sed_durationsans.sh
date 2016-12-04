b9262597ea1b3ae34d17b2fbef72565e2999ca16
RAJVARDHAN SOMRAJ DESHMUKH <RAJSD2610@users.noreply.github.com>
2016-12-04 10:34:02 +0530

    Add files via upload

---
+++ sed_durationsans.sh
@@ -0,0 +1,5 @@
+#sed s/NXST_FLOW reply (xid=0x4):/cookie, duration, table, n_packets, n_bytes, idle_age, priority, arp, in_port, vlan_tci, dl_src, dl_dst, arp_spa,arp_tpa,arp_op,actions' 
+ls |tee no1.txt
+no=`cat no1.txt | tr -s ' ' '\n' | grep -c 'newfl'`
+sed -e 's/NXST_FLOW reply (xid=0x4):/cookie,table,n_packets,n_bytes,idle_age,priority,,,,,,,,,,,,/' -e 's/arp/arp,,/' -e 's/ cookie=//' -e 's/ duration=[0-9]*.[0-9]*s,//' -e 's/ table=//' -e 's/ n_packets=//' -e 's/ n_bytes=//' -e 's/ idle_age=//' -e 's/ priority=//' -e 's/s//' <flow.$no.csv >onet.csv
+sudo chmod 777 onet.csv
