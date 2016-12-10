2d4f28dc99370e4845b84c6ab2d188b0c397b3a1
RAJVARDHAN SOMRAJ DESHMUKH <RAJSD2610@users.noreply.github.com>
2016-12-10 08:20:33 +0530

    Update snapshot.sh

--- snapshot.sh
+++ snapshot.sh
@@ -1,12 +1,20 @@
 #cd ~/Desktop/ece671/pro/u_ow/
+touch nf.txt
+chmod 777 nf.txt
 ls | tee nf.txt 
-no=`cat nf.txt | tr -s ' ' '\n'|grep -c 'newfl'`
+no=`cat nf.txt | tr -s ' ' '\n'|grep -c 'ModFl'`
+echo $no
 p=1
   while [  $p -lt $no ]; do
             # echo The counter is $p
-	     o=`expr $p - 1`
-	     echo "p:" $p "o:" $o
-	     sudo diff newfl.0.csv newfl.$p.csv | tee ~/Desktop/ece671/pro/u_ow/snaps/total.$p.csv
-             sudo diff newfl.$o.csv newfl.$p.csv | tee ~/Desktop/ece671/pro/u_ow/snaps/new_persec.$p.csv
-	     let p=p+1
+             o=`expr $p - 1`
+             echo "p:" $p "o:" $o
+             sudo diff ModFl.0.csv ModFl.$p.csv | tee ~/Desktop/ece671/pro/u_ow/snaps/total.$p.csv
+             sudo chmod 777 ~/Desktop/ece671/pro/u_ow/snaps/total.$p.csv
+             sudo sed -e 's/[0-9]c[0-9]//' -e 's/<//' -e 's/>//' -e 's/---//' -e '/^\s*$/d' -e 's/ //'<~/Desktop/ece671/pro/u_ow/snaps/total.$p.csv>~/Desktop/ece671/pr$
+             sudo diff ModFl.$o.csv ModFl.$p.csv | tee ~/Desktop/ece671/pro/u_ow/snaps/new_persec.$p.csv
+             sudo chmod 777 ~/Desktop/ece671/pro/u_ow/snaps/new_persec.$p.csv
+        #     sudo sed -e 's/[0-9]c[0-9]//' -e 's/<//' -e 's/>//' -e 's/-//' <~/Desktop/ece671/pro/u_ow/snaps/new_persec.$p.csv>
+             let p=p+1
 done
+no=0
