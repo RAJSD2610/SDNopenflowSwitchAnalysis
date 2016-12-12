ba023abd4ad31d07edaa7cf9bd3d7e7bf7de38f1
RAJVARDHAN SOMRAJ DESHMUKH <RAJSD2610@users.noreply.github.com>
2016-12-12 08:50:24 +0530

    Update TotalFlowPlot.py

--- TotalFlowPlot.py
+++ TotalFlowPlot.py
@@ -4,32 +4,40 @@ import matplotlib.pyplot as plt
 
 import seaborn
 
-path= os.path.expanduser("~/Desktop/ece671/pro/u_ow/FinSnaps")
+
+path= os.path.expanduser("~/Desktop/ece671/SwitchAnalysis/PlotDump")
 num_files = len([f for f in os.listdir(path)if os.path.isfile(os.path.join(path, f))])
 print(num_files)
 a=[]
 df=[]
 i=0
+def file_len(fname):
+    with open(fname) as f:
+        for i, l in enumerate(f):
+            pass
+    return i + 1
 while i<(num_files/2) :
  #      df+=[]
     j=i+1
-    try:
-        path="~/Desktop/ece671/SwitchAnalysis/PlotDump/ftotal."+str(j)+".csv"
+
+    path ="/home/vetri/Desktop/ece671/SwitchAnalysis/PlotDump/ftotal."+str(j)+".csv"
+    y = file_len(path)-3
 
     #    except:       pass
-        df.append(pd.read_csv(path,header=None))        
+    #df.append(pd.read_csv(path,header=None))        
 #       a+=[]
-        y=len(df[i].index)-1   #1 row added by default so that table has a entry
-        if y>0:
-            y-=2              ##1 row added by default so that table has column entry due to 7,8 at i=0
-    except:
-        y=0
+    #y=len(df[i].index)-1 #1 row added by default so that table has a entry
+    if y<0:
+        y=0 
+        
         
     a.append(y)
+    i+=1
+    
 print(a)
-plt.figure(figsize=(4, 5))
+#plt.figure(figsize=(4, 5))
 seaborn.barplot(x=list(range(1,len(a)+1)),y=a)
-plt.title("Total flow Present")
+plt.title("Total Flows Present after 1st flow")
 plt.xlabel("time(s)")
 plt.ylabel("flows")
 plt.show()
