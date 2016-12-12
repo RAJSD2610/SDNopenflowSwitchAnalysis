1c0d9cdcb2ebd72f0b5cf62c21da5b47196517f4
RAJVARDHAN SOMRAJ DESHMUKH <RAJSD2610@users.noreply.github.com>
2016-12-12 08:51:28 +0530

    Update FlowPersec.py

--- FlowPersec.py
+++ FlowPersec.py
@@ -4,28 +4,35 @@ import matplotlib.pyplot as plt
 
 import seaborn
 
+
 path= os.path.expanduser("~/Desktop/ece671/SwitchAnalysis/PlotDump")
 num_files = len([f for f in os.listdir(path)if os.path.isfile(os.path.join(path, f))])
 print(num_files)
 a=[]
 df=[]
 i=0
+def file_len(fname):
+    with open(fname) as f:
+        for i, l in enumerate(f):
+            pass
+    return i + 1
 while i<(num_files/2) :
  #      df+=[]
     j=i+1
-    try:
-        path="~/Desktop/ece671/SwitchAnalysis/PlotDump/fpersec."+str(j)+".csv"
+
+    path ="/home/vetri/Desktop/ece671/SwitchAnalysis/PlotDump/fpersec."+str(j)+".csv"
+    y = file_len(path)
 
     #    except:       pass
-        df.append(pd.read_csv(path,header=None))        
+    #df.append(pd.read_csv(path,header=None))        
 #       a+=[]
-        y=len(df[i].index)-1   #1 row added by default so that table has a entry
-        if y>0:
-            y-=1               ##1 row added by default so that table has column entry
-        if i==0:
-            y-=1               #due to 7,8 at i=0
-    except:
+    #y=len(df[i].index)-1 #1 row added by default so that table has a entry
+    
+    y-=2
+    
+    if y<0:
         y=0
+        
         
     a.append(y)
     i+=1
@@ -33,7 +40,7 @@ while i<(num_files/2) :
 print(a)
 #plt.figure(figsize=(4, 5))
 seaborn.barplot(x=list(range(1,len(a)+1)),y=a)
-plt.title("Flows Programmed Per sec")
+plt.title("Flows Programmed persec")
 plt.xlabel("time(s)")
 plt.ylabel("flows")
 plt.show()
